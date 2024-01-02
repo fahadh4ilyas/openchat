fix loss computation and deepspeed init

ochat/models/unpadded_mixtral.py
@@ -209,8 +209,7 @@ class UnpaddedMixtralAttention(nn.Module):
             cu_seqlens_q=cu_seqlens, cu_seqlens_k=cu_seqlens,
             max_seqlen_q=max_seqlen, max_seqlen_k=max_seqlen,
 
-            dropout_p=0.0, causal=True,
-            window_size=(self.sliding_window, self.sliding_window))
+            dropout_p=0.0, causal=True)
 
         # attn_output: [total_nnz, num_heads, head_dim]
         attn_output = attn_output.view(-1, self.hidden_size)  # type: ignore
@@ -260,7 +259,7 @@ class UnpaddedMixtralSparseMoeBlock(nn.Module):
 
     def forward(self, nz_hidden_states: torch.Tensor) -> torch.Tensor:
         """ """
-        batch_size, sequence_length, hidden_dim = nz_hidden_states.shape
+        sequence_length, hidden_dim = nz_hidden_states.shape
         nz_hidden_states = nz_hidden_states.view(-1, hidden_dim)
         # router_logits: (batch * sequence_length, n_experts)
         router_logits = self.gate(nz_hidden_states)
@@ -272,7 +271,7 @@ class UnpaddedMixtralSparseMoeBlock(nn.Module):
         routing_weights = routing_weights.to(nz_hidden_states.dtype)
 
         final_nz_hidden_states = torch.zeros(
-            (batch_size * sequence_length, hidden_dim), dtype=nz_hidden_states.dtype, device=nz_hidden_states.device
+            (sequence_length, hidden_dim), dtype=nz_hidden_states.dtype, device=nz_hidden_states.device
         )
 
         # One hot encode the selected experts to create an expert mask
@@ -300,7 +299,7 @@ class UnpaddedMixtralSparseMoeBlock(nn.Module):
             # However `index_add_` only support torch tensors for indexing so we'll use
             # the `top_x` tensor here.
             final_nz_hidden_states.index_add_(0, top_x, current_nz_hidden_states.to(nz_hidden_states.dtype))
-        final_nz_hidden_states = final_nz_hidden_states.reshape(batch_size, sequence_length, hidden_dim)
+        final_nz_hidden_states = final_nz_hidden_states.reshape(sequence_length, hidden_dim)
         return final_nz_hidden_states, router_logits
 
 
@@ -479,7 +478,8 @@ class MixtralForCausalLM(UnpaddedMixtralPreTrainedModel):
         max_seqlen: int,
         # Unpadded labels
         nz_shifted_label_ids: Optional[torch.Tensor] = None,
-        nz_shifted_loss_weights:      Optional[torch.Tensor] = None
+        nz_shifted_loss_weights:      Optional[torch.Tensor] = None,
+        num_seq: Optional[int] = 0
     ) -> CausalLMOutputWithPast:
         # Model logits
         hidden_states, router_logits = self.model(
@@ -494,14 +494,16 @@ class MixtralForCausalLM(UnpaddedMixtralPreTrainedModel):
         if nz_shifted_label_ids is not None:
             assert nz_shifted_loss_weights is not None
 
-            loss = weighted_cross_entropy(logits, nz_shifted_label_ids, nz_shifted_loss_weights), \
-                   weighted_token_accuracy(logits.detach(), nz_shifted_label_ids, nz_shifted_loss_weights)
-        
             aux_loss = load_balancing_loss_func(
                     router_logits, self.num_experts, self.num_experts_per_tok
                 )
             
-            loss[0] += self.router_aux_loss_coef * aux_loss
+            if num_seq > 0:
+                loss = weighted_cross_entropy(logits, nz_shifted_label_ids, nz_shifted_loss_weights) / num_seq + self.router_aux_loss_coef * aux_loss, \
+                    weighted_token_accuracy(logits.detach(), nz_shifted_label_ids, nz_shifted_loss_weights) / num_seq
+            else:
+                loss = weighted_cross_entropy(logits, nz_shifted_label_ids, nz_shifted_loss_weights) + self.router_aux_loss_coef * aux_loss, \
+                    weighted_token_accuracy(logits.detach(), nz_shifted_label_ids, nz_shifted_loss_weights)
 
         return CausalLMOutputWithPast(
             loss=loss,  # type: ignore

ochat/training_deepspeed/train.py
@@ -18,6 +18,8 @@ from ochat.config import MODEL_CONFIG_MAP
 from ochat.training_deepspeed.multipack_dataloader import MultipackDistributedDataloader
 from ochat.training_deepspeed.numpy_dataset import NumpyDataset
 
+from transformers.integrations import HfDeepSpeedConfig
+
 try:
     import deepspeed
 except ImportError:
@@ -263,6 +265,7 @@ def calculate_auto_lr(lr, batch_max_len, model_type, train_dataset):
 
 def train(args: TrainingArguments):
     deepspeed.init_distributed(dist_backend="nccl")
+    dsconfig = HfDeepSpeedConfig(args.deepspeed_config)
     RANK = dist.get_rank()
 
 
@@ -330,9 +333,7 @@ def train(args: TrainingArguments):
             batch_tensor = {k: (v.to(args.device) if v is not None else None) for k, v in batch_tensor.items()}
 
             # Update
-            loss, acc = model_engine(**batch_tensor, **batch_info).loss
-            loss = (1 / all_numseq) * loss
-            acc  = (1 / all_numseq) * acc
+            loss, acc = model_engine(**batch_tensor, **batch_info, num_seq=all_numseq).loss
 
             model_engine.backward(loss)
 
@@ -372,10 +373,10 @@ def train(args: TrainingArguments):
                     batch_tensor = {k: (v.to(args.device) if v is not None else None) for k, v in batch_tensor.items()}
 
                     # Eval
-                    eval_loss, eval_acc = model_engine(**batch_tensor, **batch_info).loss
+                    eval_loss, eval_acc = model_engine(**batch_tensor, **batch_info, num_seq=all_numseq).loss
                     
                     # Accumulate eval loss
-                    eval_total_metric.add_((1 / all_numseq) * torch.stack([eval_loss, eval_acc]))
+                    eval_total_metric.add_(torch.stack([eval_loss, eval_acc]))
                     eval_total_steps += 1
 
             # Gather eval loss (reduce sum)

ochat/training_deepspeed/train_lora_single.py
@@ -331,9 +331,7 @@ def train(args: TrainingArguments):
             batch_tensor = {k: (v.to(args.device) if v is not None else None) for k, v in batch_tensor.items()}
 
             # Update
-            loss, acc = model(**batch_tensor, **batch_info).loss
-            loss = (1 / all_numseq) * loss
-            acc  = (1 / all_numseq) * acc
+            loss, acc = model(**batch_tensor, **batch_info, num_seq=all_numseq).loss
 
             loss.backward()
 
@@ -370,10 +368,10 @@ def train(args: TrainingArguments):
                     batch_tensor = {k: (v.to(args.device) if v is not None else None) for k, v in batch_tensor.items()}
 
                     # Eval
-                    eval_loss, eval_acc = model(**batch_tensor, **batch_info).loss
+                    eval_loss, eval_acc = model(**batch_tensor, **batch_info, num_seq=all_numseq).loss
                     
                     # Accumulate eval loss
-                    eval_total_metric.add_((1 / all_numseq) * torch.stack([eval_loss, eval_acc]))
+                    eval_total_metric.add_(torch.stack([eval_loss, eval_acc]))
                     eval_total_steps += 1
 
             # Gather eval loss (reduce sum)