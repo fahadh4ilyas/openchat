7eed5ba7bd37f077158d4ede7b5ed46f1ec7e5fb
Ahmad Fahadh Ilyas <fahadh@indonesiaindicator.com>
2024-01-08 16:04:40 +0700

    add zephyr format

--- ochat/config/__init__.py
+++ ochat/config/__init__.py
@@ -174,4 +174,20 @@ MODEL_CONFIG_MAP = {
                                       prompt_format="<|im_start|>{role}\n{text}<|im_end|>",
                                       inference_condition="GPT4 Correct")
     ),
+
+    "zephyr": ModelConfig(
+        # Model
+        model_max_context=8192,
+        model_tokenizer_create=partial(transformers.AutoTokenizer.from_pretrained,
+                                       use_fast=False),  # Mistral use legacy=True https://huggingface.co/mistralai/Mistral-7B-v0.1/blob/main/tokenizer_config.json
+        model_create_for_training=partial(ochat.models.MistralForCausalLM.from_pretrained,
+                                          torch_dtype=torch.bfloat16),
+
+        # Conversation Template
+        conversation_template=partial(ChatMLConversationTemplate,
+                                      role_prefix=_chatml_role_prefix,
+                                      prompt_format="<|{role}|>\n{text}</s>",
+                                      inference_condition="")
+    ),
+
 }
