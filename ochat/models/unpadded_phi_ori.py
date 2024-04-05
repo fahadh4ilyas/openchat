87a04049fe21ee4d7a1b1c486286a1aaaf90a45e
Ahmad Fahadh Ilyas <fahadh@indonesiaindicator.com>
2024-04-05 13:27:55 +0700

    Fix phi rotary

--- ochat/models/unpadded_phi_ori.py
+++ ochat/models/unpadded_phi_ori.py
@@ -96,23 +96,26 @@ class UnpaddedPhiRotaryEmbedding(nn.Module):
         return self.cos_cached, self.sin_cached
 
 
-class UnpaddedPhiLinearScalingRotaryEmbedding(UnpaddedPhiRotaryEmbedding):
+class UnpaddedPhiLinearScalingRotaryEmbedding(torch.nn.Module):
     """PhiRotaryEmbedding extended with linear scaling. Credits to the Reddit user /u/kaiokendev"""
 
     def __init__(self, dim, max_position_embeddings=2048, base=10000, device=None, scaling_factor=1.0):
-        self.scaling_factor = scaling_factor
-        super().__init__(dim, max_position_embeddings, base, device)
+        super().__init__()
 
-    def _set_cos_sin_cache(self, seq_len, device, dtype):
-        self.max_seq_len_cached = seq_len
-        t = torch.arange(self.max_seq_len_cached, dtype=torch.int64, device=device).type_as(self.inv_freq)
-        t = t / self.scaling_factor
+        # RoPE
+        inv_freq = 1.0 / (base ** (torch.arange(0, dim, 2, dtype=torch.int64, device=device).float() / dim))
+        t = torch.arange(max_position_embeddings, dtype=torch.int64, device=device).type_as(inv_freq)
+        t = t / scaling_factor
+        freqs = torch.outer(t, inv_freq)
 
-        freqs = torch.outer(t, self.inv_freq)
         # Different from paper, but it uses a different permutation in order to obtain the same calculation
         emb = torch.cat((freqs, freqs), dim=-1)
+        dtype = torch.get_default_dtype()
         self.register_buffer("cos_cached", emb.cos().to(dtype), persistent=False)
         self.register_buffer("sin_cached", emb.sin().to(dtype), persistent=False)
+
+    def forward(self):
+        return self.cos_cached, self.sin_cached
 
 
 # Copied from transformers.models.clip.modeling_clip.CLIPMLP with CLIP->Phi
