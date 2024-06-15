7fc613d5190567b6e5a18bdbfd6369d4dc6ea734
Ahmad Fahadh Ilyas <fahadh@indonesiaindicator.com>
2024-06-15 11:25:37 +0700

    partial

--- ochat/config/model_config.py
+++ ochat/config/model_config.py
@@ -1,6 +1,7 @@
 from transformers.modeling_utils import PreTrainedModel
 from transformers.tokenization_utils import PreTrainedTokenizer
-from typing import Callable, Union, Type
+from functools import partial
+from typing import Callable, Union
 from .conversation_template import ConversationTemplate, ChatMLConversationTemplate
 
 from pydantic import BaseModel
@@ -14,7 +15,7 @@ class ModelConfig(BaseModel):
     model_create_for_training: Callable[..., PreTrainedModel]
 
     # conversation template
-    conversation_template: Union[Type[ConversationTemplate], Type[ChatMLConversationTemplate]]
+    conversation_template: Union[partial[ConversationTemplate], partial[ChatMLConversationTemplate]]
 
     class Config:
         arbitrary_types_allowed = True
