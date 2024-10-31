19673fda2cfdafb581563dbed930a2f98e75a239
Ahmad Fahadh Ilyas <fahadh@indonesiaindicator.com>
2024-10-31 18:55:34 +0700

    fix comma

--- ochat/config/__init__.py
+++ ochat/config/__init__.py
@@ -7,7 +7,7 @@ from ochat.config.model_config import ModelConfig
 from ochat.config.conversation_template import (
     Conversation,
     PretokenizedConversation,
-    PretrainingText
+    PretrainingText,
     ConversationTemplate,
     ChatMLConversationTemplate,
 )
