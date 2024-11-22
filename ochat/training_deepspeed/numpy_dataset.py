10e621fe34104cdaed1c7b229a28540e1f2fb833
Ahmad Fahadh Ilyas <fahadh@indonesiaindicator.com>
2024-11-22 20:20:42 +0700

    collect same method to utils and add pickling

--- ochat/training_deepspeed/numpy_dataset.py
+++ ochat/training_deepspeed/numpy_dataset.py
@@ -1,4 +1,6 @@
 import pyarrow.parquet as pq
+import os
+import pickle
 import orjson
 from typing import Union, Iterable
 
@@ -7,17 +9,24 @@ class NumpyDataset:
     def __init__(self, dataset_filename):
         super().__init__()
 
-        # Convert parquet to numpy for fast random access
-        table = pq.read_table(dataset_filename, memory_map=True)
-        self.dataset = {
-            k: v.to_numpy() for k, v in zip(table.column_names, table.columns)
-        }
-        self.length = table.num_rows
+        if os.path.isfile(f"{dataset_filename}.pickle"):
+            with open(f"{dataset_filename}.pickle", "rb") as f:
+                data = pickle.load(f)
+            self.dataset = data["dataset"]
+            self.length = data["length"]
+            self.metadata = data["metadata"]
+        elif os.path.isfile(f"{dataset_filename}.parquet"):
+            # Convert parquet to numpy for fast random access
+            table = pq.read_table(f"{dataset_filename}.parquet", memory_map=True)
+            self.dataset = {
+                k: v.to_numpy() for k, v in zip(table.column_names, table.columns)
+            }
+            self.length = table.num_rows
 
-        # read metadata
-        self.metadata = table.schema.metadata.get(b"metadata_json", None)
-        if self.metadata is not None:
-            self.metadata = orjson.loads(self.metadata)
+            # read metadata
+            self.metadata = table.schema.metadata.get(b"metadata_json", None)
+            if self.metadata is not None:
+                self.metadata = orjson.loads(self.metadata)
 
     def __len__(self) -> int:
         return self.length
--- ochat/training_deepspeed/train_lora_single.py
+++ ochat/training_deepspeed/train_lora_single.py
@@ -1,11 +1,6 @@
 import argparse
 import os
-import math
-import json
-import shutil
-from pathlib import Path
-from functools import partial
-from typing import Optional, Union, Literal, Dict, List
+from typing import Optional, Union, Literal, List
 
 from pydantic import BaseModel, Field, validator
 
@@ -13,12 +8,21 @@ import torch
 
 import tqdm
 import mlflow
-import numpy as np
 
 from peft import LoraConfig, get_peft_model, PeftModel, prepare_model_for_kbit_training
 
 from ochat.config import MODEL_CONFIG_MAP
-from ochat.training_deepspeed.utils import mlflow_stopper_wrapper
+from ochat.training_deepspeed.utils import (
+    mlflow_stopper_wrapper,
+    batch_to_tensor,
+    get_latest_checkpoint,
+    create_dataset,
+    calculate_auto_lr,
+    create_lr_scheduler,
+    save_openchat_metadata,
+    clean_checkpoint,
+    save_tokenizer,
+)
 from ochat.training_deepspeed.multipack_dataloader_single import MultipackDataloader
 from ochat.training_deepspeed.numpy_dataset import NumpyDataset
 
@@ -73,22 +77,6 @@ class TrainingArguments(BaseModel):
             raise ValueError("`batch_max_len` must be multiple of 2048")
 
         return v
-
-
-PAD_ID = 0
-BATCH_KEYS = {
-    "seqlens": torch.long,
-    "nz_input_ids": torch.long,
-    "nz_position_ids": torch.long,
-    "nz_shifted_label_ids": torch.long,
-    "nz_shifted_loss_weights": torch.bfloat16,
-}
-
-MODEL_LR = ["mistral", "qwen2", "mixtral"]
-
-
-def _find_multiple(a, b):
-    return (-(a // -b)) * b
 
 
 def parse_args():
@@ -162,58 +150,6 @@ def parse_args():
     return args_base
 
 
-def create_dataset(args: TrainingArguments, split_name: str):
-    # Load data
-    filename = f"{args.data_prefix}.{split_name}.parquet"
-    if not os.path.isfile(filename):
-        print(f"Skipping loading {split_name}")
-        return None
-
-    print(f"Loading {split_name} data from {filename}...")
-    return NumpyDataset(filename)
-
-
-def batch_to_tensor(batch: Dict[str, np.ndarray]):
-    # Concat batches
-    batch = {k: np.concatenate(batch[k], axis=0) for k in BATCH_KEYS.keys()}
-
-    # Pad an unused item to reach multiple of 64, for faster GEMM
-    total_seqlen = batch["nz_input_ids"].size
-    pad_len = _find_multiple(total_seqlen, 64) - total_seqlen
-
-    if pad_len > 0:
-        assert pad_len < 64
-
-        # total length
-        padding_specs = {
-            "seqlens": (1, pad_len),
-            "nz_input_ids": (pad_len, PAD_ID),
-            "nz_position_ids": (pad_len, 0),
-            "nz_shifted_label_ids": (pad_len, PAD_ID),
-            "nz_shifted_loss_weights": (pad_len, 0),
-        }
-        for k, pad_spec in padding_specs.items():
-            batch[k] = np.concatenate(
-                (batch[k], np.full(*pad_spec, dtype=batch[k].dtype)), axis=0
-            )
-
-    # to tensor
-    batch_tensor: Dict[str, torch.Tensor] = {}
-    for k, dtype in BATCH_KEYS.items():
-        batch_tensor[k] = torch.from_numpy(batch[k]).to(dtype)
-
-    # cu seqlens
-    batch_tensor["cu_seqlens"] = torch.nn.functional.pad(
-        batch_tensor["seqlens"].cumsum(-1, dtype=torch.int32), (1, 0)
-    )
-    # batch info
-    batch_info = {"max_seqlen": torch.max(batch_tensor["seqlens"]).item()}
-
-    # inputs
-    del batch_tensor["seqlens"]
-    return batch_tensor, batch_info
-
-
 def create_distributed_dataloader(args: TrainingArguments, data: NumpyDataset):
     # Multipack dataloader
     return MultipackDataloader(
@@ -224,25 +160,6 @@ def create_distributed_dataloader(args: TrainingArguments, data: NumpyDataset):
         collate_fn=batch_to_tensor,
         seed=0,
     )
-
-
-def get_latest_checkpoint(args: TrainingArguments):
-    checkpoint_list = sorted(
-        [i for i in Path(args.save_path).glob("checkpoint_*") if i.is_dir()],
-        key=lambda x: int(x.name.split("_")[-1]),
-    )
-    if checkpoint_list:
-        return str(checkpoint_list[-1])
-
-
-def clean_checkpoint(args: TrainingArguments):
-    checkpoint_list = sorted(
-        [i for i in Path(args.save_path).glob("checkpoint_*") if i.is_dir()],
-        key=lambda x: int(x.name.split("_")[-1]),
-    )
-
-    for checkpoint in checkpoint_list[: -args.max_checkpoint]:
-        shutil.rmtree(checkpoint, ignore_errors=True)
 
 
 def create_model(args: TrainingArguments):
@@ -308,85 +225,6 @@ def create_model(args: TrainingArguments):
     args.device = model.device
 
     return model, optimizer
-
-
-def cosine_schedule_with_warmup_lr_lambda(
-    current_step: int,
-    *,
-    num_warmup_steps: int,
-    num_training_steps: int,
-    min_ratio: float = 0.0,
-    num_cycles: float = 0.5,
-):
-    if current_step < num_warmup_steps:
-        return float(current_step) / float(max(1, num_warmup_steps))
-
-    progress = float(current_step - num_warmup_steps) / float(
-        max(1, num_training_steps - num_warmup_steps)
-    )
-    return min_ratio + max(
-        0.0,
-        (1 - min_ratio)
-        * 0.5
-        * (1.0 + math.cos(math.pi * float(num_cycles) * 2.0 * progress)),
-    )
-
-
-def create_lr_scheduler(args: TrainingArguments, train_total_steps: int):
-    lr_scheduler = partial(
-        cosine_schedule_with_warmup_lr_lambda,
-        num_warmup_steps=args.lr_warmup_step
-        or round(args.lr_warmup_ratio * train_total_steps),
-        num_training_steps=train_total_steps,
-        min_ratio=args.lr_min_ratio,
-    )
-
-    return lr_scheduler
-
-
-def save_tokenizer(args: TrainingArguments, save_path):
-    MODEL_CONFIG_MAP[args.model_type].model_tokenizer_create(
-        args.model_path
-    ).save_pretrained(save_path)
-
-
-def save_openchat_metadata(
-    args: TrainingArguments, epoch: Union[int, float], latest_step: int, save_path
-):
-    metadata = vars(args)
-    metadata["epoch"] = epoch
-    metadata["latest_step"] = latest_step
-
-    with open(os.path.join(save_path, "openchat.json"), "w") as f:
-        json.dump(metadata, f, default=lambda o: "<non-serializable>")
-
-
-def calculate_auto_lr(
-    base_lr: float,
-    lr: Optional[float],
-    batch_max_len: int,
-    model_type: str,
-    train_dataset: NumpyDataset,
-):
-    if lr is not None:
-        return lr
-
-    # Llama hyperparameters
-    # FIXME: Only 7B/13B is supported
-    base_bs = 4_000_000
-    if any([x in model_type.lower() for x in MODEL_LR]):
-        base_lr /= 6.0
-
-    loss_weights = np.concatenate(train_dataset["nz_shifted_loss_weights"])
-    supervised_ratio = np.sum(loss_weights != 0) / len(loss_weights)
-
-    supervised_tokens = batch_max_len * supervised_ratio
-    lr = base_lr * math.sqrt(supervised_tokens / base_bs)
-
-    print(
-        f"Use automatic learning rate {lr} (estimated from supervised ratio {supervised_ratio} effective batch size {supervised_tokens})"
-    )
-    return lr
 
 
 @mlflow_stopper_wrapper
