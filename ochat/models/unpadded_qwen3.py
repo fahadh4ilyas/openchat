e0f873db29fe01d19ebee59365161daa9399d6f4
Ahmad Fahadh Ilyas <fahadh@indonesiaindicator.com>
2025-05-14 02:34:45 +0000

    Fix output size

--- ochat/models/unpadded_qwen3.py
+++ ochat/models/unpadded_qwen3.py
@@ -177,6 +177,7 @@ class UnpaddedQwen3Attention(nn.Module):
         self.hidden_size = config.hidden_size
         self.layer_idx = layer_idx
         self.max_window_layers = config.max_window_layers
+        self.num_attention_heads = config.num_attention_heads
         self.head_dim = getattr(config, "head_dim", config.hidden_size // config.num_attention_heads)
         self.scaling = self.head_dim**-0.5
         self.sliding_window = config.sliding_window
@@ -267,7 +268,7 @@ class UnpaddedQwen3Attention(nn.Module):
             )
 
         # attn_output: [total_nnz, num_heads, head_dim]
-        attn_output = attn_output.view(-1, self.hidden_size)  # type: ignore
+        attn_output = attn_output.view(-1, self.num_attention_heads * self.head_dim)  # type: ignore
         return self.o_proj(attn_output)
 
 
