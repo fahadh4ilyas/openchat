3bc6d6082576400fd522c9e51904d7e63069e2f8
Ahmad Fahadh Ilyas <fahadh@indonesiaindicator.com>
2025-11-26 12:44:12 +0700

    Add error handling for model.save_pretrained to ensure safe serialization during training

--- ochat/training_deepspeed/train_single.py
+++ ochat/training_deepspeed/train_single.py
@@ -324,7 +324,10 @@ def train(args: TrainingArguments):
             if args.checkpoint_every > 0 and (step % args.checkpoint_every == 0):
                 save_path = os.path.join(args.save_path, f"checkpoint_{step}")
 
-                model.save_pretrained(save_path)  # type: ignore
+                try:
+                    model.save_pretrained(save_path)  # type: ignore
+                except RuntimeError:
+                    model.save_pretrained(save_path, safe_serialization=False)  # type: ignore
 
                 # Write metadata
                 save_openchat_metadata(args, epoch + 1, step, save_path)
@@ -390,7 +393,10 @@ def train(args: TrainingArguments):
             ):
                 save_path = os.path.join(args.save_path, f"ep_{epoch + 1}")
 
-                model.save_pretrained(save_path)  # type: ignore
+                try:
+                    model.save_pretrained(save_path)  # type: ignore
+                except RuntimeError:
+                    model.save_pretrained(save_path, safe_serialization=False)  # type: ignore
 
                 # Also save tokenizer from base model
                 save_tokenizer(args, save_path)
@@ -476,7 +482,10 @@ def train(args: TrainingArguments):
             ):
                 save_path = os.path.join(args.save_path, f"ep_{epoch + 1}")
 
-                model.save_pretrained(save_path)  # type: ignore
+                try:
+                    model.save_pretrained(save_path)  # type: ignore
+                except RuntimeError:
+                    model.save_pretrained(save_path, safe_serialization=False)  # type: ignore
 
                 # Also save tokenizer from base model
                 save_tokenizer(args, save_path)
