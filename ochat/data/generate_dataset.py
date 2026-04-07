da083f3804862541fdf4a19800eeb859a93e065e
Ahmad Fahadh Ilyas <fahadh@indonesiaindicator.com>
2026-04-07 15:15:48 +0700

    Fix model.has_processor

--- ochat/data/generate_dataset.py
+++ ochat/data/generate_dataset.py
@@ -135,7 +135,7 @@ def convert_conversation_batch(job_id: int, batch: list, args: DataArguments):
 
     image_processor = None
     video_processor = None
-    if model_config.has_processor:
+    if model_config.model_has_processor:
         if hasattr(tokenizer, "tokenizer") and tokenizer.tokenizer is not None:
             conv_template = model_config.conversation_template(tokenizer=tokenizer.tokenizer)
         else:
